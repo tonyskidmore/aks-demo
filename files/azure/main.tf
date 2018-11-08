428af306d381f34fb77cdb6fe2c1aedf2a93a3b4
Tony Skidmore <anthony.skidmore@accenture.com>
2018-11-08 09:17:00 +0000

    adding swapsize

--- files/azure/main.tf
+++ files/azure/main.tf
@@ -167,7 +167,7 @@ resource "azurerm_virtual_machine" "myterraformvm" {
     location              = "${var.location}"
     resource_group_name   = "${azurerm_resource_group.rg.name}"
     network_interface_ids = ["${azurerm_network_interface.nic.id}"]
-    vm_size               = "Standard_DS1_v2"
+    vm_size               = "Standard_DS2_v2"
 
     storage_os_disk {
         name              = "vmOsDisk"
